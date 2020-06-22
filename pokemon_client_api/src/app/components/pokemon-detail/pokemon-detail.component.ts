9fe7147b9d0098f03f008f55b33ea5808c0f391d
Qilobit <Qilobit@gmail.com>
2020-06-22 19:30:43 -0400

    Get pokemon info

--- pokemon_client_api/src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ pokemon_client_api/src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Pokemon } from 'src/app/models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-detail.component.css']
 })
 export class PokemonDetailComponent implements OnInit {
+  public pokemon: Pokemon;
+  public areas: any[] = [];
+  public loadingAreas: boolean = false;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: any) => {
+      if(params.id){
+        const url = params.id;
+        this.http.get(url)
+        .subscribe((resp: any) => {
+          
+          this.pokemon = new Pokemon( resp );
+
+          this.getLocationAreas(this.pokemon.location_area_encounters);
+
+        }, (err) => {
+          alert('Error getting pokemon :(');
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  getLocationAreas(url: string){
+    this.loadingAreas = true;
+    this.http.get(url)
+    .subscribe((areas: any) => {
+      this.areas = areas;
+      this.loadingAreas = false;
+    }, (err) => {
+      alert('Error getting location areas');
+      this.loadingAreas = false;
+    });
   }
 
 }
---
+++ pokemon_client_api/src/app/models/pokemon.model.ts
@@ -0,0 +1,20 @@
+
+export class Pokemon{
+  public id: string;
+  public height: number;
+  public weight: number;
+  public name: string;
+  public base_experience: number;
+  public abilities: any[];
+  public location_area_encounters: string;
+
+  constructor(rawData: any){
+    this.id = rawData.id;
+    this.height = rawData.height;
+    this.weight = rawData.weight;
+    this.name = rawData.name;
+    this.base_experience = rawData.base_experience;
+    this.abilities = rawData.abilities;
+    this.location_area_encounters = rawData.location_area_encounters;
+  }
+}
