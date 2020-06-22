88ff249bdd85680108c93e07c3dee718c28849eb
Qilobit <Qilobit@gmail.com>
2020-06-22 18:54:05 -0400

    Add routes

--- pokemon_client_api/src/app/app-routing.module.ts
+++ pokemon_client_api/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllPokemonsComponent } from './components/all-pokemons/all-pokemons.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'all-pokemons', component: AllPokemonsComponent, },
+  { path: 'pokemon-detail/:id', component: PokemonDetailComponent, },
+  { path: '**', redirectTo: 'all-pokemons' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
