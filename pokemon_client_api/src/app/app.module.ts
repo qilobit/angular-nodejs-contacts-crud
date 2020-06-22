a63724b85e3405f961d272a75747c87391842d92
Qilobit <Qilobit@gmail.com>
2020-06-22 18:54:20 -0400

    Add router outlet

--- pokemon_client_api/src/app/app.module.ts
+++ pokemon_client_api/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AllPokemonsComponent } from './components/all-pokemons/all-pokemons.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { HttpClientModule } from "@angular/common/http";
+import { PokeCardComponent } from './components/poke-card/poke-card.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllPokemonsComponent,
+    PokemonDetailComponent,
+    PokeCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
